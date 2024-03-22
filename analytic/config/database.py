4b01043f1b63c84c2c5fa77ebdc23aab98e9cfc5
Denny Danuwijaya <danudenny@gmail.com>
2024-03-22 11:04:27 +0700

    add python dockerfile

--- analytic/config/database.py
+++ analytic/config/database.py
@@ -1,9 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from loguru import logger
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Password123!@localhost:5432/kkpr_db"
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../.env")
+
+load_dotenv(
+    dotenv_path=path
+)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_DATABASE = os.getenv("DB_DATABASE")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -13,7 +28,6 @@ try:
 except Exception as e:
     logger.error(f"Database connection failed: {e}")
     raise e
-
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
